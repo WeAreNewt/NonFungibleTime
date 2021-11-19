46b9d22d06d46471f77d1f8bf2d7ec440231a1b2
JosepBove <josep@aave.com>
2021-11-19 14:07:10 +0100

    NFT Basics

--- contracts/TimeCollection.sol
+++ contracts/TimeCollection.sol
@@ -2,17 +2,63 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract TimeCollection is ERC721 {
-    constructor() ERC721("Experiments Time", "ETIME") {
+contract TimeCollection is ERC721URIStorage, ERC721Pausable, AccessControlEnumerable{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) {
+        _setBaseURI("ipfs://");
+
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
     }
-    
-    function onERC721Received(
-        address operator,
+
+    function mint(string memory tokenURI)
+    public
+    returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function burn(uint256 tokenId)
+    public
+    {
+        _burn(tokenId);
+    }
+
+    function pause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "TimeCollection: Must have pauser role to pause");
+        _pause();
+    }
+
+    function unpause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "TimeCollection: Must have pauser role to unpause");
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(
         address from,
-        uint256 tokenId,
-        bytes calldata data) external override returns (bytes4) {
-        return IERC721Receiver.onERC721Received.selector;
+        address to,
+        uint256 tokenId
+    ) internal virtual override(ERC721, ERC721Enumerable, ERC721Pausable) {
+        super._beforeTokenTransfer(from, to, tokenId);
     }
 }
