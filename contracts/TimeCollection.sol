4d74d3961e069f93731fb0763a9689c276c69de2
JosepBove <josep@aave.com>
2021-12-13 14:41:35 +0100

    Add marketplace capabilities

--- contracts/TimeCollection.sol
+++ contracts/TimeCollection.sol
@@ -13,22 +13,107 @@ contract TimeCollection is ERC721URIStorage, Ownable {
 
     Counters.Counter private _tokenIds;
 
+    string public collectionName;
+    string public collectionNameSymbol;
+
+    mapping(uint256 => Token) public allTokens;
+
     event MintedNFT(uint256 indexed tokenId, string tokenURI);
+
+    struct Token {
+        uint256 tokenId;
+        string tokenName;
+        string tokenURI;
+        address payable mintedBy;
+        address payable currentOwner;
+        address payable previousOwner;
+        uint256 price;
+        uint256 numberOfTransfers;
+        bool forSale;
+    }
 
     constructor(
         string memory name,
         string memory symbol
     ) ERC721(name, symbol)
     {
+        collectionName = name;
+        collectionNameSymbol = symbol;
     }
 
     function mint(string memory name, string memory description, string memory work, string memory time, string memory date) public {
+        require(msg.sender != address(0));
         uint256 newItemId = _tokenIds.current();
         string memory tokenURI = formatTokenURI(name, description, work, time, date);
         _safeMint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
+        Token memory newToken = Token(
+            newItemId,
+            name,
+            tokenURI,
+            payable(msg.sender),
+            payable(msg.sender),
+            payable(address(0)),
+            0,
+            0,
+            false
+        );
+        allTokens[newItemId] = newToken;
         _tokenIds.increment();
         emit MintedNFT(newItemId, tokenURI);
+    }
+
+
+    function buyToken(uint256 _tokenId) public payable {
+        require(msg.sender != address(0));
+        require(_exists(_tokenId));
+        address tokenOwner = ownerOf(_tokenId);
+        require(tokenOwner != address(0));
+        require(tokenOwner != msg.sender);
+        Token memory token = allTokens[_tokenId];
+        require(msg.value >= token.price);
+        require(token.forSale);
+        _transfer(tokenOwner, msg.sender, _tokenId);
+        address payable sendTo = token.currentOwner;
+        sendTo.transfer(msg.value);
+        token.previousOwner = token.currentOwner;
+        token.currentOwner = payable(msg.sender);
+        token.numberOfTransfers += 1;
+        allTokens[_tokenId] = token;
+    }
+
+    function changeTokenPrice(uint256 _tokenId, uint256 _newPrice) public {
+        require(msg.sender != address(0));
+        require(_exists(_tokenId));
+        address tokenOwner = ownerOf(_tokenId);
+        require(tokenOwner == msg.sender);
+        Token memory token = allTokens[_tokenId];
+        token.price = _newPrice;
+        allTokens[_tokenId] = token;
+    }
+
+    function toggleForSale(uint256 _tokenId) public {
+        require(msg.sender != address(0));
+        require(_exists(_tokenId));
+        address tokenOwner = ownerOf(_tokenId);
+        require(tokenOwner == msg.sender);
+        Token memory token = allTokens[_tokenId];
+        if(token.forSale) {
+            token.forSale = false;
+        } else {
+            token.forSale = true;
+        }
+        allTokens[_tokenId] = token;
+    }
+
+    function getTokenMetaData(uint _tokenId) public view returns(string memory) {
+        string memory tokenMetaData = tokenURI(_tokenId);
+        return tokenMetaData;
+    }
+
+    function getTokenOwner(uint256 _tokenId) public view returns(address) {
+        address _tokenOwner = ownerOf(_tokenId);
+        return _tokenOwner;
     }
 
     function formatTokenURI(string memory name, string memory description, string memory work, string memory time, string memory date) private pure returns (string memory) {
