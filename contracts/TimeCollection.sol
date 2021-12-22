52fdf77dbc42a3e06f0797b613a2058919117121
JosepBove <josep@aave.com>
2021-12-22 19:29:50 +0100

    Change version

--- contracts/TimeCollection.sol
+++ contracts/TimeCollection.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Undefined
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
