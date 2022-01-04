b9618ed0fe637fefcbf4fbab211d47ad9e4aa198
Andrew Schmidt <andrew@aave.com>
2022-01-03 22:14:33 -0600

    fix: home page styling

--- packages/frontend/src/modules/Home/index.tsx
+++ packages/frontend/src/modules/Home/index.tsx
@@ -3,20 +3,38 @@ import { Link } from 'react-router-dom'
 
 export const Home = () => {
     return (
-        <div className="h-screen flex justify-between">
+        <div className="flex h-full justify-between overflow-hidden">
             <div className="w-1/2 flex flex-col p-5">
-                <div>Earn money on your time</div>
-                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quia maxime sapiente voluptate nam eum. Incidunt tempora reiciendis sint minima.</div>
-                <div className="flex justify-evenly">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 m-9 border border-blue-700 rounded">
-                        <Link to="/profile">Mint time NFT</Link>
-                    </button>
-                    <button className="bg-white hover:bg-blue-100 text-blue-500 font-bold py-2 px-3 m-9 border border-blue-700 rounded">
-                        <Link to="/marketplace">Explore marketplace</Link>
-                    </button>
+                <div>
+                    <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                        <div className="sm:text-center lg:text-left">
+                            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                                <span className="block xl:inline">Earn money</span>
+                                <span className="block text-indigo-600">on your time</span>
+                            </h1>
+                            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                            </p>
+                            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                                <div className="rounded-md shadow">
+                                    <Link to={'/profile/mint'} className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                                        Mint Time NFT
+                                    </Link>
+                                </div>
+                                <div className="mt-3 sm:mt-0 sm:ml-3">
+                                    <Link to={'/marketplace'} className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                                        Explore Marketplace
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+
                 </div>
             </div >
-            <div className="w1/2 mx-auto">Image</div>
+            <div className="w-1/2 h-full flex flex-col">
+                <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="" />
+            </div>
         </div >
     )
 }
