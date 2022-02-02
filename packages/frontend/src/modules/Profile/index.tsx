36b0eaa38f9d2dbee640556b8fee3ab5db47f68b
Andrew Schmidt <andrew@aave.com>
2022-02-02 11:23:10 -0600

    fix: visting direct link on preview deploy

--- packages/frontend/src/modules/Profile/index.tsx
+++ packages/frontend/src/modules/Profile/index.tsx
@@ -305,7 +305,7 @@ export default function Profile() {
                                             </div>
                                         </div>
                                         <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                                            <button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white sm:ml-3 sm:w-auto sm:text-sm" onClick={() => { navigator.clipboard.writeText(location.pathname) }}>
+                                            <button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white sm:ml-3 sm:w-auto sm:text-sm" onClick={() => { navigator.clipboard.writeText(baseUrl + location.pathname) }}>
                                                 Copy URL
                                             </button>
                                             <button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onClick={() => setShareProfileModalOpen(false)}>
