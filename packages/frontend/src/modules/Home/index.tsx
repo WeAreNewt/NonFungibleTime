cb2ed04b6bc522ed7b466015b665e4adb4bf5264
Andrew Schmidt <andrew@aave.com>
2022-02-02 12:45:11 -0600

    feat: update landing page image

--- packages/frontend/src/modules/Home/index.tsx
+++ packages/frontend/src/modules/Home/index.tsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAppDataProvider } from '../../lib/providers/app-data-provider'
+import LandingPageClock from '../../images/LandingPageClock.jpg';
 
 export const Home = () => {
     const { currentAccount } = useAppDataProvider();
     return (
-        <div className="flex h-full justify-between overflow-hidden">
+        <div className="flex justify-between">
             <div className="w-1/2 flex flex-col p-5">
                 <div>
                     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                         <div className="sm:text-center lg:text-left">
-                            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white sm:text-5xl md:text-6xl">
                                 <span className="block xl:inline">Earn money</span>
                                 <span className="block text-indigo-600">on your time</span>
                             </h1>
-                            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-                            </p>
                             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                 <div className="rounded-md shadow">
                                     <Link to={'/profile/' + currentAccount} className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
@@ -34,8 +32,8 @@ export const Home = () => {
 
                 </div>
             </div >
-            <div className="w-1/2 h-full flex flex-col">
-                <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="" />
+            <div className="w-1/2">
+                <img className="w-full object-cover" src={LandingPageClock} alt="landing page clock" />
             </div>
         </div >
     )
