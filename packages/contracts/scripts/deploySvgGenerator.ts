af3666a44554cd2b0543a2aff59e9bac82d048ca
donosonaumczuk <adonoso@itba.edu.ar>
2022-02-07 14:18:03 -0300

    SVGs working, code improved

--- packages/contracts/scripts/deploySvgGenerator.ts
+++ packages/contracts/scripts/deploySvgGenerator.ts
@@ -1,11 +1,36 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-  const SvgGeneratorFactory = await ethers.getContractFactory('SvgGenerator');
-  const svgGenerator = await SvgGeneratorFactory.deploy();
+  const FirstSvgGeneratorFactory = await ethers.getContractFactory('FirstCirclesSvgGenerator');
+  const firstSvgGenerator = await FirstSvgGeneratorFactory.deploy();
+  await firstSvgGenerator.deployed();
 
+  const ThirdSvgGeneratorFactory = await ethers.getContractFactory('ThirdCircleSvgGenerator');
+  const thirdSvgGenerator = await ThirdSvgGeneratorFactory.deploy();
+  await thirdSvgGenerator.deployed();
+
+  const LastSvgGeneratorFactory = await ethers.getContractFactory('LastCirclesSvgGenerator');
+  const lastSvgGenerator = await LastSvgGeneratorFactory.deploy();
+  await lastSvgGenerator.deployed();
+
+  const SvgGeneratorFactory = await ethers.getContractFactory('SvgGenerator');
+  const svgGenerator = await SvgGeneratorFactory.deploy(
+    firstSvgGenerator.address,
+    thirdSvgGenerator.address,
+    lastSvgGenerator.address
+  );
   await svgGenerator.deployed();
-  console.log('SvgGenerator deployed to:', svgGenerator.address);
+
+  console.log(
+    'SVG: ',
+    svgGenerator.address,
+    'Fst',
+    firstSvgGenerator.address,
+    'Trd',
+    thirdSvgGenerator.address,
+    'Lst',
+    lastSvgGenerator.address
+  );
 }
 
 main().catch((error) => {
