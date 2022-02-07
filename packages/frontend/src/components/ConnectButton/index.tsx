2e9d6eb18c623487b1bb5a44f2a5d5c51e4364a2
Andrew Schmidt <andrew@aave.com>
2022-02-06 21:01:54 -0600

    feat: go to profile screen when clicking on connected wallet

--- packages/frontend/src/components/ConnectButton/index.tsx
+++ packages/frontend/src/components/ConnectButton/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { formatEthAddress } from '../../lib/helpers/format';
 import { useWeb3, WalletType } from '../../lib/providers/web3-provider';
 
 export default function AddressInfo() {
   const { connect, account, isCorrectChain, active, requestToSwitchChain } = useWeb3();
-
-  const onConnect = async () => {
+  const navigate = useNavigate();
+  const onClick = async () => {
+    console.log('clickd')
     try {
-      if (active && !isCorrectChain) {
-        requestToSwitchChain();
-        return;
+      if (active) {
+        if (!isCorrectChain) {
+          requestToSwitchChain();
+          return;
+        } else {
+          console.log('actie')
+          navigate('/profile/' + account)
+        }
+      } else {
+        // For now, defaults to injected provider
+        connect(WalletType.Values.injected);
       }
 
-      // For now, defaults to metamask
-      connect(WalletType.Values.injected);
     } catch (err) {
       // TODO: Add error toaster
       alert('Error on connecting wallet: ' + (err as Error).message);
@@ -21,7 +29,7 @@ export default function AddressInfo() {
   };
 
   return (
-    <div onClick={onConnect} className="px-4">
+    <div onClick={onClick} className="px-4">
       <div className="w-full flex items-center justify-center px-6 py-1 border border-transparent text-base font-semibold rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-2 md:text-lg md:px-8 cursor-pointer">
         {account
           ? isCorrectChain
