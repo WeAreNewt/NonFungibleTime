97edd6077d600f5f9475afa8e0d47824ac1f6fbe
Lucas Munhoz <ln.munhoz@gmail.com>
2022-02-08 12:25:57 -0300

    fix: mint modal margins & paddings fix for desktop

--- packages/frontend/src/components/Forms/Label.tsx
+++ packages/frontend/src/components/Forms/Label.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 
 export const Label = (
   props: React.DetailedHTMLProps<React.LabelHTMLAttributes<HTMLLabelElement>, HTMLLabelElement>
-) => <label className="block text-sm font-medium text-gray-700" {...props} />;
+) => <label className="block text-sm font-medium text-gray-700 mb-1" {...props} />;
