d14bd512e6c7be7d8c40f515215826c4b17d6419
Lucas Munhoz <ln.munhoz@gmail.com>
2022-02-08 18:56:14 -0300

    feat: category filter + query

--- packages/frontend/src/modules/Marketplace/index.tsx
+++ packages/frontend/src/modules/Marketplace/index.tsx
@@ -1,33 +1,43 @@
 import { useSubscription } from '@apollo/client';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { FaSpinner } from 'react-icons/fa';
+import CategoryFilter from '../../components/CategoryFilter';
 import NFTCard from '../../components/NFTCard';
 import { NFTGrid } from '../../components/NFTGrid';
-import { NftsDocument } from '../../lib/graphql/index';
+import { NftsDocument, Nft_Filter } from '../../lib/graphql/index';
 import { NFT } from '../../types';
 
 export default function Marketplace() {
-  const { data, loading } = useSubscription(NftsDocument);
+  const [category, setCategory] = useState('Show All');
+  const whereArg = useMemo<Nft_Filter | undefined>(() => {
+    if (category === 'Show All') return undefined;
+
+    return {
+      category,
+    };
+  }, [category]);
+
+  const { data, loading } = useSubscription(NftsDocument, {
+    variables: {
+      first: 100,
+      where: whereArg,
+    },
+  });
   const nfts: NFT[] | undefined = data && data.nfts ? data.nfts : undefined;
 
   return (
-    <div className="bg-slate-100 dark:bg-black">
-      <div className="flex flex-col max-w-7xl m-auto p-10">
-        <div className="flex flex-row justify-between">
+    <div className="bg-slate-100 dark:bg-black p-10">
+      <div className="flex flex-col max-w-7xl m-auto">
+        <div className="flex flex-col gap-4 md:flex-row md:gap-0 justify-between items-center mb-10">
           {/** Marketplace Header */}
-          <div className="w-1/2 justify-items-start">
+          <div className="w-full md:w-2/3 justify-items-start">
             <div className="items-center text-gray-900 dark:text-white text-4xl font-extrabold">
               Explore marketplace
             </div>
           </div>
 
-          {/** Filter, TODO: Selector instead of button*/}
-          <div className="w-1/4 p-5 justify-items-end">
-            <div className="items-center">
-              <div className="items-center justify-center px-6 py-1 border border-gray text-base font-semibold rounded-md text-black bg-white hover:bg-gray-400 md:py-2 md:text-lg md:px-8 cursor-pointer">
-                Show All
-              </div>
-            </div>
+          <div className="w-full md:w-1/3 justify-items-end ">
+            <CategoryFilter onSelect={setCategory} selected={category} />
           </div>
         </div>
         {loading || !nfts ? (
