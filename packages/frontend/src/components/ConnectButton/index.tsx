535120bb7d7112134fbe249b190cccb756293111
Kevin O'Connell <kevinoconnell42@gmail.com>
2022-02-08 22:22:03 -0600

    wallet now disconnects (#125)
    * wallet now disconnects
    
    * small error fix
    
    * import error
    
    * feat: switch modal to dropdown
    
    Co-authored-by: Andrew Schmidt <andrew@aave.com>

--- packages/frontend/src/components/ConnectButton/index.tsx
+++ packages/frontend/src/components/ConnectButton/index.tsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 import { formatEthAddress } from '../../lib/helpers/format';
 import { useWeb3, WalletType } from '../../lib/providers/web3-provider';
 
 export default function AddressInfo() {
-  const { connect, account, isCorrectChain, active, requestToSwitchChain } = useWeb3();
-  const navigate = useNavigate();
+  const { connect, account, isCorrectChain, active, requestToSwitchChain, disconnect } = useWeb3();
+  const [isOpen, setIsOpen] = useState(false)
   const onClick = async () => {
-    console.log('clickd');
     try {
       if (active) {
         if (!isCorrectChain) {
           requestToSwitchChain();
           return;
         } else {
-          console.log('actie');
-          navigate('/profile/' + account);
+          setIsOpen(!isOpen)
         }
       } else {
         // For now, defaults to injected provider
@@ -26,16 +23,16 @@ export default function AddressInfo() {
       alert('Error on connecting wallet: ' + (err as Error).message);
     }
   };
-
   return (
-    <div onClick={onClick} className="px-4">
-      <div className="w-full flex items-center justify-center px-6 py-1 border border-transparent text-base font-semibold rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-2 md:text-lg md:px-8 cursor-pointer">
+    <div>
+      <div onClick={onClick} className="w-full flex items-center justify-center px-6 py-1 border border-transparent text-base font-semibold rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-2 md:text-lg md:px-8 cursor-pointer">
         {account
           ? isCorrectChain
             ? formatEthAddress(account)
             : 'Wrong Network'
           : 'Connect Wallet'}
       </div>
+      {isOpen && <div className="absolute top-100 w-full flex items-center justify-center px-6 py-1 border text-base font-semibold rounded-md  hover:bg-slate-200 md:py-2 md:text-lg md:px-8 border-1 border-black bg-white text-black cursor-pointer" onClick={() => { disconnect(); setIsOpen(false) }}>Disconnect</div>}
     </div>
   );
 }
