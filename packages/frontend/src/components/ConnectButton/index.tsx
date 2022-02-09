7f85f7413b190775ddf7291cf87a7c4ac3e2df54
Kevin O'Connell <kevinoconnell42@gmail.com>
2022-02-08 21:20:33 -0600

    wallet now disconnects

--- packages/frontend/src/components/ConnectButton/index.tsx
+++ packages/frontend/src/components/ConnectButton/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import { Dialog } from '@headlessui/react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { formatEthAddress } from '../../lib/helpers/format';
 import { useWeb3, WalletType } from '../../lib/providers/web3-provider';
 
 export default function AddressInfo() {
-  const { connect, account, isCorrectChain, active, requestToSwitchChain } = useWeb3();
+  const { connect, account, isCorrectChain, active, requestToSwitchChain, disconnect } = useWeb3();
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false)
   const onClick = async () => {
     console.log('clickd');
     try {
@@ -15,7 +17,7 @@ export default function AddressInfo() {
           return;
         } else {
           console.log('actie');
-          navigate('/profile/' + account);
+          setIsOpen(true)
         }
       } else {
         // For now, defaults to injected provider
@@ -27,7 +29,59 @@ export default function AddressInfo() {
     }
   };
 
+
   return (
+    <>
+    <Dialog
+    as="div"
+    open={isOpen}
+    className="fixed inset-0 z-10 overflow-y-auto"
+    onClose={() => setIsOpen(false)}
+  >
+    <div className="min-h-screen px-4 text-center">
+  
+      {/* This element is to trick the browser into centering the modal contents. */}
+      <span
+        className="inline-block h-screen align-middle"
+        aria-hidden="true"
+      >
+        &#8203;
+      </span>
+     
+        <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+          <Dialog.Title
+            as="h3"
+            className="text-lg font-medium leading-6 text-gray-900"
+          >
+            Are you sure you want to disconnect your wallet?
+          </Dialog.Title>
+  
+          <div className="flex flex-row ml-14 mt-5">
+
+  <div className="basis-1/2">
+            <button
+              type="button"
+              className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+              onClick={() => { 
+                disconnect()
+                setIsOpen(false)}}
+            >
+              Yes
+            </button>
+            </div>
+            <div className="basis-1/2">
+            <button
+              type="button"
+              className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+              onClick={() => setIsOpen(false)}
+            >
+              No
+            </button>
+            </div>
+          </div>
+        </div>
+    </div>
+  </Dialog>
     <div onClick={onClick} className="px-4">
       <div className="w-full flex items-center justify-center px-6 py-1 border border-transparent text-base font-semibold rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-2 md:text-lg md:px-8 cursor-pointer">
         {account
@@ -37,5 +91,6 @@ export default function AddressInfo() {
           : 'Connect Wallet'}
       </div>
     </div>
+    </>
   );
 }
