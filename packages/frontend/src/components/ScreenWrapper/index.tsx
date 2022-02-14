f288204f0258d8138e5083e5ec2b76cce6593484
Joaquin Battilana <battilanajn@gmail.com>
2022-02-14 15:32:16 -0300

    fixed footer and centered home for small devices

--- packages/frontend/src/components/ScreenWrapper/index.tsx
+++ packages/frontend/src/components/ScreenWrapper/index.tsx
@@ -9,7 +9,7 @@ interface ScreenWrapperProps {
 export default function ScreenWrapper({ children }: ScreenWrapperProps) {
   return (
     <div className="bg-slate-100 dark:bg-black flex flex-col">
-      <div className="min-h-screen grow">
+      <div className="min-h-screen flex flex-col">
         <Navbar />
         {children}
       </div>
