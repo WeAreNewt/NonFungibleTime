8c9b2cfd85c21c62198892ed026661aef87bd17f
donosonaumczuk <adonoso@itba.edu.ar>
2022-02-15 18:58:56 -0300

    Button component press feedback added

--- packages/frontend/src/components/Button/index.tsx
+++ packages/frontend/src/components/Button/index.tsx
@@ -17,11 +17,11 @@ const baseStyles = 'flex items-center gap-2 py-2 px-4 cursor-pointer font-medium
 const variantStyles: Record<ButtonVariant, string> = {
   [ButtonVariant.PRIMARY]: classnames(
     baseStyles,
-    'border border-transparent text-white bg-indigo-600 hover:bg-indigo-700 '
+    'border border-transparent text-white bg-indigo-600 hover:bg-indigo-700 active:bg-indigo-800'
   ),
   [ButtonVariant.SECONDARY]: classnames(
     baseStyles,
-    'bg-white hover:bg-gray-100 text-gray-700 border border-gray-300'
+    'bg-white hover:bg-gray-200 active:bg-gray-300 text-gray-700 border border-gray-300'
   ),
 };
 
